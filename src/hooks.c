715fa53949d7bf9a7935e8e978caa2ee529bb36b
jdagoy <jdagoy@student.s19.be>
2024-01-08 11:09:08 +0100

    decompose player wall collision functions

--- src/hooks.c
+++ src/hooks.c
@@ -11,6 +11,23 @@ int	check_playerpos(t_game *game)
 	return (1);
 }
 
+int movement_y_offset(t_game *game, char *type, int value)
+{
+	int res;
+
+	res = 0;
+	if (value < 0)
+		res = 0;
+	else if (ft_strncmp(type, "add_x", ft_strlen(type)) == 0 || \
+				ft_strncmp(type, "sub_x", ft_strlen(type)) == 0)
+		res = game->map_width;
+	else if (ft_strncmp(type, "add_y", ft_strlen(type)) == 0 || \
+				ft_strncmp(type, "sub_y", ft_strlen(type)) == 0)
+		res = game->map_height;
+	return res;
+}
+
+
 void	player_movement_y(int keycode, t_game *game)
 {
 	int	add_x;
@@ -22,36 +39,16 @@ void	player_movement_y(int keycode, t_game *game)
 		return ;
 	add_x = (game->player.position.axis[X] - game->player.front.dir[X]) / TILE_SIZE - 0.2;
 	if (add_x < 0 || add_x > game->map_width)
-	{
-		if (add_x < 0)
-			add_x = 0;
-		else
-			add_x = game->map_width;
-	}
+		add_x = movement_y_offset(game, "add_x", add_x);
 	add_y = (game->player.position.axis[Y] - game->player.front.dir[Y]) / TILE_SIZE - 0.2;
 	if (add_y < 0 || add_y > game->map_height)
-	{
-		if (add_y < 0)
-			add_y = 0;
-		else
-			add_y = game->map_height;
-	}
+		add_y = movement_y_offset(game, "add_y", add_y);
 	sub_x = (game->player.position.axis[X] + game->player.front.dir[X]) / TILE_SIZE + 0.2;
 	if (sub_x < 0 || sub_x > game->map_width)
-	{
-		if (sub_x < 0)
-			sub_x = 0;
-		else
-			sub_x = game->map_width;
-	}
+		sub_x = movement_y_offset(game, "sub_x", sub_x);
 	sub_y = (game->player.position.axis[Y] + game->player.front.dir[Y]) / TILE_SIZE + 0.2;
 	if (sub_y < 0 || sub_y > game->map_height)
-	{
-		if (sub_y < 0)
-			sub_y = 0;
-		else
-			sub_y = game->map_height;
-	}
+		sub_y = movement_y_offset(game, "sub_y", sub_y);
 	if (keycode == K_W)
 	{
 		if (game->map[game->player.array_y][add_x] == '0')
@@ -75,6 +72,8 @@ void	player_movement_x(int keycode, t_game *game)
 	int	sub_x;
 	int	sub_y;
 
+	if (check_playerpos(game) == 0)
+		return ;
 	add_x = (game->player.position.axis[X] + game->player.front.dir[Y]) / TILE_SIZE - 0.1;
 	if (add_x < 0 || add_x > game->map_width)
 	{
@@ -123,6 +122,37 @@ void	player_movement_x(int keycode, t_game *game)
 	}
 }
 
+void	open_door(int keycode, t_game *game)
+{
+
+	int	add_x;
+	int	add_y;
+
+	add_x = (game->player.position.axis[X] - game->player.front.dir[X]) / TILE_SIZE - 0.5;
+	if (add_x < 0 || add_x > game->map_width)
+	{
+		if (add_x < 0)
+			add_x = 0;
+		else
+			add_x = game->map_width;
+	}
+	add_y = (game->player.position.axis[Y] - game->player.front.dir[Y]) / TILE_SIZE - 0.5;
+	if (add_y < 0 || add_y > game->map_height)
+	{
+		if (add_y < 0)
+			add_y = 0;
+		else
+			add_y = game->map_height;
+	}
+	if (keycode == K_E)
+	{
+		printf("x:%d y:%d\n", add_x, game->player.array_y);
+		if (game->player.array_y >= 0 && game->player.array_y < game->map_height && add_x >= 0 && add_x < game->map_width)
+			if (game->map[game->player.array_y][add_x] == '3')
+				game->map[game->player.array_y][add_x] = '0';
+
+	}
+}
 
 int	keybindings(int keycode, t_game *game)
 {
@@ -136,6 +166,8 @@ int	keybindings(int keycode, t_game *game)
 		player_movement_y(keycode, game);
 	else if (keycode == K_A || keycode == K_D)
 		player_movement_x(keycode, game);
+	else if (keycode == K_E)
+		open_door(keycode, game);
 	else if (keycode == K_RIGHT)
 	{
 		game->player.angle -= 0.1;
